d56bca8f2a9184e201439f2849b95fc25735b0dd
Emmo00 <nwaforemmanuel005@gmail.com>
2025-11-27 18:06:33 +0100

    Enhance CollateralManager and ICollateralManager interfaces with liquidation threshold and collateral factor management functions

--- src/CollateralManager.sol
+++ src/CollateralManager.sol
@@ -11,67 +11,100 @@ import {ILendingPool} from "./interfaces/ILendingPool.sol";
  * @dev Manages collateral deposits, withdrawals, and valuations in a lending protocol.
  */
 contract CollateralManager is ICollateralManager {
-    uint public collateralFactor;
+    uint256 public collateralFactor;
+    uint256 public liquidationThreshold;
     uint256 private constant SCALE = 1e18;
     uint256 private constant HEALTH_FACTOR_THRESHOLD = 1 * SCALE;
 
     IPriceOracle public immutable priceOracle;
 
-    mapping(address => uint256) private _collaterals;
+    mapping(address => uint256) private _collateralBalance;
     ILendingPool public immutable lendingPool;
 
-    constructor(address _priceOracle, address _lendingPool, uint256 _collateralFactor) {
+    modifier onlyLendingPool() {
+        require(msg.sender == address(lendingPool), "CollateralManager: Caller is not the lending pool");
+        _;
+    }
+
+    constructor(address _priceOracle, address _lendingPool, uint256 _collateralFactor, uint256 _liquidationThreshold) {
         require(_priceOracle != address(0), "CollateralManager: Invalid price oracle address");
         require(_lendingPool != address(0), "CollateralManager: Invalid lending pool address");
         require(_collateralFactor > 0 && _collateralFactor <= SCALE, "CollateralManager: Invalid collateral factor");
+        require(
+            _liquidationThreshold > 0 && _liquidationThreshold <= SCALE,
+            "CollateralManager: Invalid liquidation threshold"
+        );
 
         priceOracle = IPriceOracle(_priceOracle);
         lendingPool = ILendingPool(_lendingPool);
         collateralFactor = _collateralFactor;
+        liquidationThreshold = _liquidationThreshold;
     }
 
-    function depositCollateral(address onBehalfOf) external payable override {
+    function depositCollateral(address onBehalfOf) external payable override onlyLendingPool {
         require(msg.value > 0, "CollateralManager: Deposit amount must be greater than zero");
-        _collaterals[onBehalfOf] += msg.value;
+        _collateralBalance[onBehalfOf] += msg.value;
 
         emit CollateralDeposited(onBehalfOf, msg.value);
     }
 
-    function withdrawCollateral(uint256 amount) external override {
+    function withdrawCollateral(address user, uint256 amount, address to) external override onlyLendingPool {
         require(amount > 0, "CollateralManager: Withdraw amount must be greater than zero");
-        require(_collaterals[msg.sender] >= amount, "CollateralManager: Insufficient collateral");
-        require(
-            lendingPool.healthFactorAfterWithdraw(msg.sender, amount) > HEALTH_FACTOR_THRESHOLD,
-            "CollateralManager: Cannot withdraw collateral that would lead to undercollateralization"
-        );
+        require(_collateralBalance[user] >= amount, "CollateralManager: Insufficient collateral");
+        // TODO: make sure the health factor is checked
+        // require(
+        //     lendingPool.healthFactorAfterWithdraw(user, amount) > HEALTH_FACTOR_THRESHOLD,
+        //     "CollateralManager: Cannot withdraw collateral that would lead to undercollateralization"
+        // );
 
-        _collaterals[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
+        _collateralBalance[user] -= amount;
+        payable(to).transfer(amount);
+        emit CollateralWithdrawn(user, amount);
+    }
 
-        emit CollateralWithdrawn(msg.sender, amount);
+    function seizeCollateral(address user, uint256 amount, address liquidator) external override onlyLendingPool {
+        require(amount > 0, "CollateralManager: Seize amount must be greater than zero");
+
+        uint256 userBalance = _collateralBalance[user];
+        uint256 seizeAmount = amount > userBalance ? userBalance : amount;
+
+        _collateralBalance[user] -= seizeAmount;
+        payable(liquidator).transfer(seizeAmount);
+
+        emit CollateralSeized(user, seizeAmount, liquidator);
     }
 
     function getUserCollateral(address user) external view override returns (uint256) {
-        return _collaterals[user];
+        return _collateralBalance[user];
     }
 
     function getCollateralValue(address user) public view override returns (uint256) {
+        uint256 userCollateral = _collateralBalance[user];
+        if (userCollateral == 0) {
+            return 0;
+        }
         uint256 ethPrice = priceOracle.getEthPrice();
-        return (_collaterals[user] * ethPrice) / SCALE;
+        return (userCollateral * ethPrice) / SCALE;
     }
 
-    function getCollateralizableValue(address user) external view override returns (uint256) {
+    function getMaxBorrow(address user) external view override returns (uint256) {
         uint256 collateralValue = getCollateralValue(user);
         return (collateralValue * collateralFactor) / SCALE;
     }
 
-    function seizeCollateral(address user, uint256 amount, address liquidator) external override {
-        require(amount > 0, "CollateralManager: Seize amount must be greater than zero");
-        require(_collaterals[user] >= amount, "CollateralManager: Insufficient collateral to seize");
+    function getLiquidationThreshold() external view override returns (uint256) {
+        return liquidationThreshold;
+    }
 
-        _collaterals[user] -= amount;
-        payable(liquidator).transfer(amount);
+    function setCollateralFactor(uint256 newFactor) external override onlyLendingPool {
+        require(newFactor > 0 && newFactor <= SCALE, "CollateralManager: Invalid collateral factor");
+        collateralFactor = newFactor;
+        emit CollateralFactorUpdated(newFactor);
+    }
 
-        emit CollateralSeized(user, amount, liquidator);
+    function setLiquidationThreshold(uint256 newThreshold) external override onlyLendingPool {
+        require(newThreshold > 0 && newThreshold <= SCALE, "CollateralManager: Invalid liquidation threshold");
+        liquidationThreshold = newThreshold;
+        emit LiquidationThresholdUpdated(newThreshold);
     }
 }
--- src/interfaces/ICollateralManager.sol
+++ src/interfaces/ICollateralManager.sol
@@ -13,10 +13,12 @@ interface ICollateralManager {
     function depositCollateral(address onBehalfOf) external payable;
 
     /**
-     * @dev Withdraws collateral for the caller.
+     * @dev Withdraws collateral for a user.
+     * @param user The address of the user whose collateral is being withdrawn.
      * @param amount The amount of collateral to withdraw.
+     * @param to The address to which the withdrawn collateral will be sent.
      */
-    function withdrawCollateral(uint256 amount) external;
+    function withdrawCollateral(address user, uint256 amount, address to) external;
 
     /**
      * @dev Gets the total collateral of a user.
@@ -37,7 +39,7 @@ interface ICollateralManager {
      * @param user The address of the user.
      * @return The collateralizable value of the user.
      */
-    function getCollateralizableValue(address user) external view returns (uint256);
+    function getMaxBorrow(address user) external view returns (uint256);
 
     /**
      * @dev Seizes collateral from a user and transfers it to a liquidator(asset liquidation).
@@ -47,23 +49,56 @@ interface ICollateralManager {
      */
     function seizeCollateral(address user, uint256 amount, address liquidator) external;
 
+    /**
+     * @dev Gets the current collateral factor.
+     * @return The collateral factor.
+     */
+    function getLiquidationThreshold() external view returns (uint256);
+
+    /**
+     * @dev Sets a new collateral factor.
+     * @param newFactor The new collateral factor to set.
+     */
+    function setCollateralFactor(uint256 newFactor) external;
+
+    /**
+     * @dev Sets a new liquidation threshold.
+     * @param newThreshold The new liquidation threshold to set.
+     */
+    function setLiquidationThreshold(uint256 newThreshold) external;
+
     /////// Events ///////
-    /* @dev Emitted when collateral is deposited.
+    /**
+     * @dev Emitted when collateral is deposited.
      * @param user The address of the user who deposited collateral.
      * @param amount The amount of collateral deposited.
      */
     event CollateralDeposited(address indexed user, uint256 amount);
 
-    /* @dev Emitted when collateral is withdrawn.
+    /**
+     * @dev Emitted when collateral is withdrawn.
      * @param user The address of the user who withdrew collateral.
      * @param amount The amount of collateral withdrawn.
      */
     event CollateralWithdrawn(address indexed user, uint256 amount);
 
-    /* @dev Emitted when collateral is seized.
+    /**
+     * @dev Emitted when collateral is seized.
      * @param user The address of the user whose collateral was seized.
      * @param amount The amount of collateral seized.
      * @param liquidator The address of the liquidator who received the seized collateral.
      */
     event CollateralSeized(address indexed user, uint256 amount, address indexed liquidator);
+
+    /**
+     * @dev Emitted when the collateral factor is updated.
+     * @param newFactor The new collateral factor.
+     */
+    event CollateralFactorUpdated(uint256 newFactor);
+
+    /**
+     * @dev Emitted when the liquidation threshold is updated.
+     * @param newThreshold The new liquidation threshold.
+     */
+    event LiquidationThresholdUpdated(uint256 newThreshold);
 }
