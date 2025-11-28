d33dc35ecd71090856d65ec32cb146f0ea5a8937
Emmo00 <nwaforemmanuel005@gmail.com>
2025-11-28 12:06:06 +0100

    Refactor PriceOracle: implement Ownable inheritance; add event for price updates; enforce price validation in constructor and setEthPrice function.

--- src/PriceOracleMock.sol
+++ src/PriceOracleMock.sol
@@ -1,22 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IPriceOracle} from "./interfaces/IPriceOracle.sol";
-
 /**
  * @title Price Oracle
  * @author Emmo00
  * @notice get the price of eth against the borrow asset of the lending pool
  */
-contract PriceOracle is IPriceOracle {
-    uint256 private ethPrice;
+import {IPriceOracle} from "./interfaces/IPriceOracle.sol";
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
 
-    constructor(uint256 _initialPrice) {
+contract PriceOracle is IPriceOracle, Ownable {
+    uint256 private ethPrice;
+    
+    event PriceUpdated(uint256 newPrice);
+
+    constructor(uint256 _initialPrice) Ownable(msg.sender) {
+        require(_initialPrice > 0, "PriceOracle: invalid initial price");
         ethPrice = _initialPrice;
     }
 
-    function setEthPrice(uint256 _newPrice) external {
+    function setEthPrice(uint256 _newPrice) external onlyOwner {
+        require(_newPrice > 0, "PriceOracle: invalid price");
         ethPrice = _newPrice;
+        emit PriceUpdated(_newPrice);
     }
 
     function getEthPrice() external view override returns (uint256) {
